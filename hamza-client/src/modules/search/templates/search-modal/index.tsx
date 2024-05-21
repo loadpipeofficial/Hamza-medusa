b4753de5284b66a8e462f534e68762499d859337
karan chugh <karanchugh@192.168.1.6>
2024-05-21 10:21:45 +0530

    ci: added back icon in search page

--- hamza-client/src/modules/search/templates/search-modal/index.tsx
+++ hamza-client/src/modules/search/templates/search-modal/index.tsx
@@ -9,7 +9,7 @@ import Hit from '@modules/search/components/hit';
 import Hits from '@modules/search/components/hits';
 import SearchBox from '@modules/search/components/search-box';
 import { useEffect, useRef } from 'react';
-
+import { IoMdArrowRoundBack } from 'react-icons/io';
 export default function SearchModal({
     closeModal,
 }: {
@@ -70,21 +70,31 @@ export default function SearchModal({
         <div className="relative z-[75]">
             <div className="fixed inset-0 bg-opacity-75 backdrop-blur-md opacity-100 h-screen w-screen" />
             <div className="fixed inset-0 px-5 sm:p-0" ref={searchRef}>
-                <div className="flex flex-col justify-start w-full h-fit transform p-5 items-center text-left align-middle transition-all max-h-[75vh] bg-transparent shadow-none">
-                    <InstantSearch
-                        indexName={SEARCH_INDEX_NAME}
-                        searchClient={searchClient}
-                    >
-                        <div className="flex absolute flex-col h-fit w-full sm:w-fit">
-                            <div className="w-full flex items-center gap-x-2 p-4 bg-[rgba(3,7,18,0.5)] text-ui-fg-on-color backdrop-blur-2xl rounded-rounded">
-                                <MagnifyingGlassMini />
-                                <SearchBox />
+                <div className="flex">
+                    <div className="pt-7 pl-5 ">
+                        <IoMdArrowRoundBack
+                            onClick={(e) => {
+                                closeModal();
+                            }}
+                            className="!text-white text-3xl cursor-pointer"
+                        />
+                    </div>
+                    <div className="flex flex-col justify-start w-full h-fit transform p-5 items-center text-left align-middle transition-all max-h-[75vh] bg-transparent shadow-none">
+                        <InstantSearch
+                            indexName={SEARCH_INDEX_NAME}
+                            searchClient={searchClient}
+                        >
+                            <div className="flex absolute flex-col h-fit w-full sm:w-fit">
+                                <div className="w-full flex items-center gap-x-2 p-4 bg-[rgba(3,7,18,0.5)] text-ui-fg-on-color backdrop-blur-2xl rounded-rounded">
+                                    <MagnifyingGlassMini />
+                                    <SearchBox />
+                                </div>
+                                <div className="flex-1 mt-6">
+                                    <Hits hitComponent={Hit} />
+                                </div>
                             </div>
-                            <div className="flex-1 mt-6">
-                                <Hits hitComponent={Hit} />
-                            </div>
-                        </div>
-                    </InstantSearch>
+                        </InstantSearch>
+                    </div>
                 </div>
             </div>
         </div>
