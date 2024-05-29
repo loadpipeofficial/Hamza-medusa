23fb7c886f4625fa250cd1abea6cc0e98b91eef7
Garo <garonazarian09@gmail.com>
2024-05-29 21:53:01 +0700

    (WIP) Avg Rating / Review Count Client templating

--- hamza-client/src/modules/products/components/product-price/index.tsx
+++ hamza-client/src/modules/products/components/product-price/index.tsx
@@ -4,6 +4,8 @@ import {
     PricedVariant,
 } from '@medusajs/medusa/dist/types/pricing';
 import { clx } from '@medusajs/ui';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import {
     formatCryptoPrice,
@@ -11,6 +13,7 @@ import {
 } from '@lib/util/get-product-price';
 import { RegionInfo } from 'types/global';
 import { useCustomerAuthStore } from '@store/customer-auth/customer-auth';
+const BACKEND_URL = process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL;
 
 export default function ProductPrice({
     product,
@@ -34,8 +37,51 @@ export default function ProductPrice({
         return <div className="block w-32 h-9 bg-gray-100 animate-pulse" />;
     }
 
+    console.log(`Product is ${JSON.stringify(product)}`);
+    useEffect(() => {
+        const fetchReviewCount = async () => {
+            try {
+                const response = await axios.post(
+                    `${BACKEND_URL}/custom/review/count`,
+                    {
+                        product_id: product.id,
+                    }
+                );
+                setReviewCount(response.data.count); // Assuming the response contains the count directly
+            } catch (error) {
+                console.error('Failed to fetch review count:', error);
+            }
+        };
+
+        const fetchAverageRating = async () => {
+            try {
+                const response = await axios.post(
+                    `${BACKEND_URL}/custom/review/average`,
+                    {
+                        product_id: product.id,
+                    }
+                );
+                setAverageRating(response.data.average); // Assuming the response contains the average directly
+            } catch (error) {
+                console.error('Failed to fetch average rating:', error);
+            }
+        };
+
+        fetchReviewCount();
+        fetchAverageRating();
+    }, [product.id]);
+
+    const [averageRating, setAverageRating] = useState(0);
+    const [reviewCount, setReviewCount] = useState(0);
+
     return (
         <div className="flex flex-col space-y-1 text-ui-fg-base text-white">
+            <div>
+                <h3>Product Reviews</h3>
+                <p className="text-white">
+                    Average Rating: {averageRating} ({reviewCount} reviews)
+                </p>
+            </div>
             {preferredPrice ? (
                 <span className={clx('text-xl-semi')}>
                     {formatCryptoPrice(
