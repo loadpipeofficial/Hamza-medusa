7ff0f89b1952d1ce68de52f07831accf1db90a55
Acer <john.kosinski@gmail.com>
2024-05-30 11:30:16 +0700

    removed stripe and manual payment methods

--- hamza-server/medusa-config.js
+++ hamza-server/medusa-config.js
@@ -35,7 +35,6 @@ const REDIS_URL = process.env.REDIS_URL || 'redis://localhost:6379';
 
 const plugins = [
     `medusa-fulfillment-manual`,
-    `medusa-payment-manual`,
     {
         resolve: `@medusajs/file-local`,
         options: {
