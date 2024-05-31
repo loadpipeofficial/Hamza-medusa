d735009e6fbc00ffddc5c390663cc34630fca346
Garo <garonazarian09@gmail.com>
2024-05-31 16:48:06 +0800

    Review already exists alert

--- hamza-client/src/modules/review/[id]/review-template.tsx
+++ hamza-client/src/modules/review/[id]/review-template.tsx
@@ -111,9 +111,7 @@ const ReviewTemplate = () => {
                 <Button
                     className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                     onClick={submitReview}
-                    disabled={
-                        !canSubmit || rating === 0 || review.trim() === ''
-                    }
+                    disabled={rating === 0 || review.trim() === ''}
                 >
                     Submit Review
                 </Button>
