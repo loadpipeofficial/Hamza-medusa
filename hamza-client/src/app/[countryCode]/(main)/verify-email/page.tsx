258222e3e4c4004ab6e34fadfef3ef719841080c
karan chugh <karanchugh@192.168.1.13>
2024-06-05 15:25:50 +0530

    ci: added google auth verification

--- hamza-client/src/app/[countryCode]/(main)/verify-email/page.tsx
+++ hamza-client/src/app/[countryCode]/(main)/verify-email/page.tsx
@@ -6,6 +6,7 @@ import axios from 'axios';
 import { useState } from 'react';
 import { Toast } from '@medusajs/ui';
 import { useRouter } from 'next/navigation';
+import getGoogleOAuthURL from '@lib/util/google-url';
 
 const VerifyEmail = () => {
     const { status, customer_id } = useCustomerAuthStore();
@@ -66,6 +67,22 @@ const VerifyEmail = () => {
                                 Verify
                             </button>
                         </form>
+                        <div className="text-white text-md text-center p-4">
+                            Or
+                        </div>
+                        <div className="buttons">
+                            <a href={getGoogleOAuthURL()}>
+                                <button className="px-4 py-2 border flex gap-2 border-slate-200 dark:border-slate-700 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150">
+                                    <img
+                                        className="w-6 h-6"
+                                        src="https://www.svgrepo.com/show/475656/google-color.svg"
+                                        loading="lazy"
+                                        alt="google logo"
+                                    />
+                                    <span>Verify with Google</span>
+                                </button>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
