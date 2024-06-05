2aa2eeecae0c6ebffde4dd2061d0bd5ba2c585fb
Garo <garonazarian09@gmail.com>
2024-06-05 14:51:15 +0700

    Reverting last migration

--- hamza-server/src/models/product.ts
+++ hamza-server/src/models/product.ts
@@ -1,17 +1,9 @@
-import {
-    Column,
-    Entity,
-    Index,
-    JoinColumn,
-    ManyToOne,
-    OneToMany,
-} from 'typeorm';
+import { Column, Entity, Index, JoinColumn, ManyToOne } from 'typeorm';
 import {
     // alias the core entity to not cause a naming conflict
     Product as MedusaProduct,
 } from '@medusajs/medusa';
 import { Store } from './store';
-import { ProductReview } from './product-review';
 
 @Entity()
 export class Product extends MedusaProduct {
@@ -21,10 +13,6 @@ export class Product extends MedusaProduct {
     @ManyToOne(() => Store)
     @JoinColumn({ name: 'store_id', referencedColumnName: 'id' })
     store?: Store;
-
-    @OneToMany(() => ProductReview, (review) => review.product)
-    @JoinColumn({ name: 'review' })
-    review?: ProductReview[];
 
     @Column()
     massmarket_prod_id?: string;
