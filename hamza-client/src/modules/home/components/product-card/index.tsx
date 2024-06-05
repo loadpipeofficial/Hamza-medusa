8e17b48c847822ab1177a5b38ebc71a11bd1da0a
Jonatahn Bajada <jbajada@ryerson.ca>
2024-06-05 22:58:32 +0700

    added container to cards

--- hamza-client/src/modules/home/components/product-card/index.tsx
+++ hamza-client/src/modules/home/components/product-card/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ProductCard from './components/product-card';
-import { Box, SimpleGrid } from '@chakra-ui/react';
+import { Box, SimpleGrid, Container } from '@chakra-ui/react';
 import products from './data/data';
 
 const ProductCardGroup = () => {
@@ -11,23 +11,25 @@ const ProductCardGroup = () => {
     //Hovering over it could reveal a buy or cart button?
     return (
         <Box my="8" p="8">
-            <SimpleGrid
-                columns={[1, 2, 3, 4, 5]}
-                spacing="1.25rem"
-                rowGap="2.5rem"
-                placeItems="center"
-            >
-                {products.map((product) => (
-                    <ProductCard
-                        key={product.id}
-                        productName={product.productName}
-                        productPrice={product.productPrice}
-                        imageSrc={product.imageSrc}
-                        hasDiscount={product.hasDiscount}
-                        discountValue={product.discountValue}
-                    />
-                ))}
-            </SimpleGrid>
+            <Container maxW="1440px">
+                <SimpleGrid
+                    columns={[1, 2, 3, 4, 5]}
+                    spacing="1.25rem"
+                    rowGap="2.5rem"
+                    placeItems="center"
+                >
+                    {products.map((product) => (
+                        <ProductCard
+                            key={product.id}
+                            productName={product.productName}
+                            productPrice={product.productPrice}
+                            imageSrc={product.imageSrc}
+                            hasDiscount={product.hasDiscount}
+                            discountValue={product.discountValue}
+                        />
+                    ))}
+                </SimpleGrid>
+            </Container>
         </Box>
     );
 };
