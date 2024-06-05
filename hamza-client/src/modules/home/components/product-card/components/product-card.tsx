7174d99140d1326ed94c5d5449df602d0e661d40
Jonatahn Bajada <jbajada@ryerson.ca>
2024-06-05 22:31:19 +0700

    clean up components

--- hamza-client/src/modules/home/components/product-card/components/product-card.tsx
+++ hamza-client/src/modules/home/components/product-card/components/product-card.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React from 'react';
 import { Card, CardBody, Image, Text, Flex, Box } from '@chakra-ui/react';
 import {
     TiStarOutline,
