5b786e1f27513fecf5662fc4f616acfd897cd425
Jonatahn Bajada <jbajada@ryerson.ca>
2024-06-10 13:39:08 +0700

    updated hover on nav links

--- hamza-client/src/modules/layout/templates/nav-update/nav-link/nav-link.tsx
+++ hamza-client/src/modules/layout/templates/nav-update/nav-link/nav-link.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link';
 import { useParams } from 'next/navigation';
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, Box, Flex } from '@chakra-ui/react';
 
 interface NavLinkProps {
@@ -12,14 +12,27 @@ interface NavLinkProps {
 
 const NavLink: React.FC<NavLinkProps> = ({ href, title }) => {
     const { countryCode } = useParams();
+    const [isHovered, setIsHovered] = useState(false);
+
+    const handleMouseEnter = () => setIsHovered(true);
+    const handleMouseLeave = () => setIsHovered(false);
 
     return (
         <Link
             style={{ borderWidth: '1px', borderColor: 'transparent' }}
             href={`/${countryCode}${href}`}
         >
-            <Flex h="100%" p="4">
-                <Text alignSelf={'center'} fontSize={'20px'} color="white">
+            <Flex
+                h="100%"
+                p="4"
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+            >
+                <Text
+                    alignSelf={'center'}
+                    fontSize={'20px'}
+                    color={isHovered ? '#94D42A' : 'white'}
+                >
                     {title}
                 </Text>
             </Flex>
