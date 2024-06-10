74a4ca1651efdbba0f84e7ce2ee24e800e1fa500
Garo <garonazarian09@gmail.com>
2024-06-10 17:31:03 +0700

    styling fixes

--- hamza-client/src/modules/home/components/hero-banner/index.tsx
+++ hamza-client/src/modules/home/components/hero-banner/index.tsx
@@ -4,13 +4,7 @@ import { FaArrowRightLong } from 'react-icons/fa6';
 
 const HeroBanner = () => {
     return (
-        <Flex
-            px={6}
-            pt={8}
-            justifyContent={'center'}
-            alignItems={'center'}
-            flexDirection={'column'}
-        >
+        <>
             <Flex
                 w="100%"
                 bg="blue.400"
@@ -26,44 +20,52 @@ const HeroBanner = () => {
                     </Text>
                 </Link>
             </Flex>
-            <Text
-                mb={{ sm: 3, md: '0' }}
-                lineHeight={'62px'}
-                color={'#FBFDFA'}
-                style={{ fontSize: '32px' }}
-            >
-                Buy & Sell Products Using{' '}
-                <span style={{ color: '#7B61FF' }}>
-                    <b>Crypto</b>
-                </span>{' '}
-                as a Community
-            </Text>
-            <b />
             <Flex
-                gap="10px"
-                flexDirection={{
-                    base: 'column',
-                    sm: 'column',
-                    md: 'row',
-                    lg: 'row',
-                }}
-                marginRight={{
-                    sm: 'auto',
-                    md: '0',
-                    lg: '0',
-                }}
+                px={6}
+                pt={8}
+                justifyContent={'center'}
+                alignItems={'center'}
+                flexDirection={'column'}
             >
-                <Text color={'#FBFDFA'} style={{ fontSize: '20px' }}>
-                    By The <b>People</b>
+                <Text
+                    mb={{ sm: 3, md: '0' }}
+                    lineHeight={'62px'}
+                    color={'#FBFDFA'}
+                    style={{ fontSize: '32px' }}
+                >
+                    Buy & Sell Products Using{' '}
+                    <span style={{ color: '#7B61FF' }}>
+                        <b>Crypto</b>
+                    </span>{' '}
+                    as a Community
                 </Text>
-                <Box marginRight={{ sm: 'auto' }} alignSelf="center">
-                    <FaArrowRightLong size={24} color="#FBFDFA" />
-                </Box>
-                <Text color={'#FBFDFA'} style={{ fontSize: '20px' }}>
-                    For The <b>People</b> Using Blockchain Tech
-                </Text>
+                <b />
+                <Flex
+                    gap="10px"
+                    flexDirection={{
+                        base: 'column',
+                        sm: 'column',
+                        md: 'row',
+                        lg: 'row',
+                    }}
+                    marginRight={{
+                        sm: 'auto',
+                        md: '0',
+                        lg: '0',
+                    }}
+                >
+                    <Text color={'#FBFDFA'} style={{ fontSize: '20px' }}>
+                        By The <b>People</b>
+                    </Text>
+                    <Box marginRight={{ sm: 'auto' }} alignSelf="center">
+                        <FaArrowRightLong size={24} color="#FBFDFA" />
+                    </Box>
+                    <Text color={'#FBFDFA'} style={{ fontSize: '20px' }}>
+                        For The <b>People</b> Using Blockchain Tech
+                    </Text>
+                </Flex>
             </Flex>
-        </Flex>
+        </>
     );
 };
 
