b47d73506320f290ef27880f04a184d1335fb6cc
Jonatahn Bajada <jbajada@ryerson.ca>
2024-06-10 10:42:44 +0700

    updated card layout responsiveness

--- hamza-client/src/app/[countryCode]/(main)/layout.tsx
+++ hamza-client/src/app/[countryCode]/(main)/layout.tsx
@@ -1,18 +1,19 @@
-import { Metadata } from "next"
-import Footer from "@modules/layout/templates/footer"
-import Nav from "@modules/layout/templates/nav"
-const MEDUSA_CLIENT_URL = process.env.NEXT_PUBLIC_MEDUSA_CLIENT_URL || "https://localhost:8000"
+import { Metadata } from 'next';
+import Footer from '@modules/layout/templates/footer';
+import Nav from '@modules/layout/templates/nav';
+const MEDUSA_CLIENT_URL =
+    process.env.NEXT_PUBLIC_MEDUSA_CLIENT_URL || 'https://localhost:8000';
 
 export const metadata: Metadata = {
-  metadataBase: new URL(MEDUSA_CLIENT_URL),
-}
+    metadataBase: new URL(MEDUSA_CLIENT_URL),
+};
 
 export default async function PageLayout(props: { children: React.ReactNode }) {
-  return (
-    <>
-      <Nav />
-      {props.children}
-      <Footer />
-    </>
-  )
+    return (
+        <>
+            <Nav />
+            {props.children}
+            <Footer />
+        </>
+    );
 }
