5c6c73ddc3df5fc41690df5086fdcdf86f9deb43
Jonatahn Bajada <jbajada@ryerson.ca>
2024-06-11 18:53:23 +0700

    building store v2

---
+++ hamza-client/src/modules/store-v2/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Flex, Box, Container, Text } from '@chakra-ui/react';
+
+const StoreTemplate = () => {
+    return (
+        <Container>
+            <Flex maxWidth={'1280px'}></Flex>
+        </Container>
+    );
+};
+
+export default StoreTemplate;
---
+++ hamza-client/src/modules/store/components/product-layout/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Box, Flex, Container } from '@chakra-ui/react';
+import ProductCard from '@modules/products/components/product-card';
+
+const index = () => {
+    return <div>index</div>;
+};
+
+export default index;
