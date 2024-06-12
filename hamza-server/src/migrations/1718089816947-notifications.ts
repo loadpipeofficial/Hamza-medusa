3f936a637364df4d5fa63976c529ba2f0ffcc1d9
Garo <garonazarian09@gmail.com>
2024-06-12 12:46:52 +0700

    Notification Schema working

--- hamza-server/src/migrations/1718089816947-notifications.ts
+++ hamza-server/src/migrations/1718089816947-notifications.ts
@@ -2,9 +2,9 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Notifications1718089816947 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
-        // Create customer_notifications table
+        // Create customer_notification table
         await queryRunner.query(`
-            CREATE TABLE "customer_notifications"
+            CREATE TABLE "customer_notification"
             (
                 "id"                varchar                  NOT NULL,
                 "customer_id"       varchar                  NOT NULL,
@@ -19,6 +19,6 @@ export class Notifications1718089816947 implements MigrationInterface {
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE "customer_notifications"`);
+        await queryRunner.query(`DROP TABLE "customer_notification"`);
     }
 }
--- hamza-server/src/services/customer-notification.ts
+++ hamza-server/src/services/customer-notification.ts
@@ -2,7 +2,7 @@ import { Lifetime } from 'awilix';
 import { Logger, TransactionBaseService } from '@medusajs/medusa';
 import { CustomerNotificationRepository } from '../repositories/customer-notification';
 import { NotificationTypeRepository } from '../repositories/notification-type';
-import { NotificationType } from '../models/notification-type';
+// import { NotificationType } from '../models/notification-type';
 
 const notificationTypes = [
     { name: 'order_shipped' },
@@ -30,67 +30,53 @@ class CustomerNotificationSerivce extends TransactionBaseService {
         this.notificationTypeRepository = container.notificationTypeRepository;
     }
 
-    async createNotificationTypes(): Promise<NotificationType[]> {
-        const notificationTypes = [
-            { name: 'order_shipped' },
-            { name: 'posted a new product' },
-            { name: 'order status changed' },
-            { name: 'promotions/discounts' },
-            { name: 'survey_update' },
-            { name: 'sms' },
-            { name: 'email' },
-            { name: 'LINE' },
-            { name: 'whatsapp' },
-        ];
+    // TODO: Will this be useful for the full implementation when we have two tables later?
 
-        const createdNotificationTypes: NotificationType[] = [];
+    // async createNotificationTypes(): Promise<NotificationType[]> {
+    //     const notificationTypes = [
+    //         { name: 'order_shipped' },
+    //         { name: 'posted a new product' },
+    //         { name: 'order status changed' },
+    //         { name: 'promotions/discounts' },
+    //         { name: 'survey_update' },
+    //         { name: 'sms' },
+    //         { name: 'email' },
+    //         { name: 'LINE' },
+    //         { name: 'whatsapp' },
+    //     ];
+    //
+    //     const createdNotificationTypes: NotificationType[] = [];
+    //
+    //     try {
+    //         for (const type of notificationTypes) {
+    //             const notificationType =
+    //                 this.notificationTypeRepository.create(type);
+    //             const savedNotificationType =
+    //                 await this.notificationTypeRepository.save(
+    //                     notificationType
+    //                 );
+    //             createdNotificationTypes.push(savedNotificationType);
+    //         }
+    //     } catch (e) {
+    //         this.logger.error(`Error creating notification types: ${e}`);
+    //         throw e;
+    //     }
+    //
+    //     return createdNotificationTypes;
+    // }
 
+    async addNotification(customerId: string, notificationType: string) {
         try {
-            for (const type of notificationTypes) {
-                const notificationType =
-                    this.notificationTypeRepository.create(type);
-                const savedNotificationType =
-                    await this.notificationTypeRepository.save(
-                        notificationType
-                    );
-                createdNotificationTypes.push(savedNotificationType);
-            }
+            const notification = this.customerNotificationRepository.create({
+                customer_id: customerId,
+                notification_type: notificationType,
+            });
+            const notify =
+                await this.customerNotificationRepository.save(notification);
+            return notify;
         } catch (e) {
-            this.logger.error(`Error creating notification types: ${e}`);
+            this.logger.error(`Error adding notification: ${e}`);
             throw e;
-        }
-
-        return createdNotificationTypes;
-    }
-
-    async notifyOrderShipped(customer_id: string): Promise<any> {
-        try {
-            console.log('Order Shipped');
-
-            // Find the notification type for "order_shipped"
-            const notificationType =
-                await this.notificationTypeRepository.findOne({
-                    where: { name: 'order_shipped' },
-                });
-
-            if (!notificationType) {
-                throw new Error('Notification type "order_shipped" not found');
-            }
-
-            // Create the customer notification
-            const customerNotification =
-                this.customerNotificationRepository.create({
-                    notification_type: notificationType, // Use the correct property name
-                    customer_id: customer_id,
-                });
-
-            // Save the customer notification
-            return await this.customerNotificationRepository.save(
-                customerNotification
-            );
-        } catch (e) {
-            this.logger.error(`Error notifying order shipped: ${e}`);
-            throw e; // Rethrow the error to ensure it can be handled appropriately upstream
         }
     }
 }
