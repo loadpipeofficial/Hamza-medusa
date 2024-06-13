9bc6891a7d94ab44a293391887245255e1e297f5
Jonatahn Bajada <jbajada@ryerson.ca>
2024-06-13 15:21:56 +0700

    added range slider

--- hamza-client/src/modules/store-v2/component/dual-slider.tsx
+++ hamza-client/src/modules/store-v2/component/dual-slider.tsx
@@ -1,31 +1,49 @@
+import React, { useState } from 'react';
 import {
     Box,
-    Slider,
-    SliderTrack,
-    SliderFilledTrack,
-    SliderThumb,
-    Flex,
     Text,
+    Flex,
     Divider,
+    RangeSlider,
+    RangeSliderTrack,
+    RangeSliderFilledTrack,
+    RangeSliderThumb,
 } from '@chakra-ui/react';
-import React, { useState, useEffect } from 'react';
 
 const DualSlider = () => {
-    const [minValue, setMinValue] = useState(10);
-    const [maxValue, setMaxValue] = useState(100);
+    const [range, setRange] = useState([10, 2000]);
+
+    const handleRangeChange = (values) => {
+        setRange(values);
+    };
 
     return (
         <Box my="2rem">
-            <Slider onChange={(val) => setMinValue(val)}>
-                <SliderTrack bg="secondary.charcoal.900">
-                    <SliderFilledTrack bg="primary.indigo.900" />
-                </SliderTrack>
-                <SliderThumb boxSize={6} bg="primary.indigo.900" />
-                <SliderThumb boxSize={6} bg="primary.indigo.900" />
-            </Slider>
+            <RangeSlider
+                aria-label={['min', 'max']}
+                defaultValue={[10, 2000]}
+                min={0}
+                max={2000}
+                onChange={handleRangeChange}
+                colorScheme="blue"
+            >
+                <RangeSliderTrack bg="secondary.charcoal.900">
+                    <RangeSliderFilledTrack bg="primary.indigo.900" />
+                </RangeSliderTrack>
+                <RangeSliderThumb
+                    index={0}
+                    boxSize={6}
+                    bg="primary.indigo.900"
+                />
+                <RangeSliderThumb
+                    index={1}
+                    boxSize={6}
+                    bg="primary.indigo.900"
+                />
+            </RangeSlider>
 
             {/* Display for min and max values */}
-            <Flex mt="1rem" justifyContent={'center'}>
+            <Flex mt="1rem" justifyContent="center">
                 <Flex
                     borderColor="secondary.davy.900"
                     flexDirection="column"
@@ -41,7 +59,7 @@ const DualSlider = () => {
                         Minimum
                     </Text>
                     <Text fontSize="18px" lineHeight="1">
-                        USD {minValue}
+                        USD {range[0]}
                     </Text>
                 </Flex>
                 <Divider
@@ -64,7 +82,7 @@ const DualSlider = () => {
                         Maximum
                     </Text>
                     <Text fontSize="18px" lineHeight="1">
-                        USD {maxValue}
+                        USD {range[1]}
                     </Text>
                 </Flex>
             </Flex>
