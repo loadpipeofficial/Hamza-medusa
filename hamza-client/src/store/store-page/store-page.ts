73b35a9d1fa351df8297fe5e6bcdfa2d9d237909
Jonatahn Bajada <jbajada@ryerson.ca>
2024-06-13 17:37:02 +0700

    added currency global states

--- hamza-client/src/store/store-page/store-page.ts
+++ hamza-client/src/store/store-page/store-page.ts
@@ -3,12 +3,16 @@ import create from 'zustand';
 // Define the state and associated actions in an interface
 interface StoreState {
     categorySelect: string | null;
+    currencySelect: string | null;
     setCategorySelect: (item: string | null) => void;
+    setCurrencySelect: (item: string | null) => void;
 }
 
 // Create the Zustand store
 const useStorePage = create<StoreState>((set) => ({
-    categorySelect: null, // initial state for selected item
+    categorySelect: null,
+    currencySelect: null,
+    setCurrencySelect: (item: string | null) => set({ categorySelect: item }),
     setCategorySelect: (item: string | null) => set({ categorySelect: item }),
 }));
 
