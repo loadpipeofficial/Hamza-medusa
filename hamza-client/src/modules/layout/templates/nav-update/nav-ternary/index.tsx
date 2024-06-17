1290090aee5a8903931c825d05626c09b8be93c4
Garo <garonazarian09@gmail.com>
2024-06-17 17:34:11 +0700

    Is Verified check

--- hamza-client/src/modules/layout/templates/nav-update/nav-ternary/index.tsx
+++ hamza-client/src/modules/layout/templates/nav-update/nav-ternary/index.tsx
@@ -5,17 +5,19 @@ import { useCustomerAuthStore } from '@store/customer-auth/customer-auth';
 import NavLink from '@modules/layout/templates/nav-update/nav-link/nav-link';
 
 const NavTernary = () => {
-    const { customer_id } = useCustomerAuthStore();
+    const { customer_id, is_verified } = useCustomerAuthStore();
 
     return (
         <React.Fragment>
-            {!customer_id ? (
-                <React.Fragment></React.Fragment>
-            ) : (
+            {customer_id ? (
                 <React.Fragment>
                     <NavLink href="/account" title="Account" />
-                    <NavLink href="/verify-email" title="Verify Account" />
+                    {!is_verified && (
+                        <NavLink href="/verify-email" title="Verify Account" />
+                    )}
                 </React.Fragment>
+            ) : (
+                <React.Fragment></React.Fragment>
             )}
         </React.Fragment>
     );
