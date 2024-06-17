a166e96555d4a3dfd54c155f58042e5e149c059c
Garo <garonazarian09@gmail.com>
2024-06-17 17:22:38 +0700

    Non-logged in user /account /verify

---
+++ hamza-client/src/modules/layout/templates/nav-update/nav-ternary/index.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import React from 'react';
+import { useCustomerAuthStore } from '@store/customer-auth/customer-auth';
+import NavLink from '@modules/layout/templates/nav-update/nav-link/nav-link';
+
+const NavTernary = () => {
+    const { customer_id } = useCustomerAuthStore();
+
+    return (
+        <React.Fragment>
+            {!customer_id ? (
+                <React.Fragment></React.Fragment>
+            ) : (
+                <React.Fragment>
+                    <NavLink href="/account" title="Account" />
+                    <NavLink href="/verify-email" title="Verify" />
+                </React.Fragment>
+            )}
+        </React.Fragment>
+    );
+};
+
+export default NavTernary;
