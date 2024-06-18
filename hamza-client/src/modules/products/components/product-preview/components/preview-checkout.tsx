56b873d23bb3a8705f657dd1325cc25b44d49ce9
Jonatahn Bajada <jbajada@ryerson.ca>
2024-06-18 13:14:45 +0700

    can now see variant sizes and colors

--- hamza-client/src/modules/products/components/product-preview/components/preview-checkout.tsx
+++ hamza-client/src/modules/products/components/product-preview/components/preview-checkout.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
-import { Text, Card, Button, Flex } from '@chakra-ui/react';
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import { Text, Card, Button, Flex, Select } from '@chakra-ui/react';
 import useProductPreview from '@store/product-preview/product-preview';
 
 const PreviewCheckout = () => {
+    const [sizes, setSizes] = useState<string[]>([]);
+    const [colors, setColors] = useState<string[]>([]);
+
     const { productData } = useProductPreview();
 
-    console.log(productData);
+    const getUniqueOptions = (
+        variants: Variant[],
+        optionIndex: number
+    ): string[] => {
+        const optionsSet = new Set<string>(
+            variants.map((variant) => variant.title.split(' / ')[optionIndex])
+        );
+        return Array.from(optionsSet);
+    };
+
+    useEffect(() => {
+        if (productData && productData.variants) {
+            setSizes(getUniqueOptions(productData.variants, 0));
+            setColors(getUniqueOptions(productData.variants, 1));
+        }
+    }, [productData]);
+
     return (
         <Card
             padding="2rem"
@@ -17,7 +38,35 @@ const PreviewCheckout = () => {
         >
             <Text color="primary.green.900">Listing Price</Text>
             <Text color="primary.green.900">Listing Price</Text>
+            <Flex flexDirection="column" my="2rem">
+                <Text color="white">Size:</Text>
+                <Select placeholder="Select size">
+                    {sizes.length > 0 ? (
+                        sizes.map((size) => (
+                            <option key={size} value={size}>
+                                {size}
+                            </option>
+                        ))
+                    ) : (
+                        <option>No sizes available</option>
+                    )}
+                </Select>
+            </Flex>
 
+            <Flex flexDirection="column" my="2rem">
+                <Text color="white">Color:</Text>
+                <Select placeholder="Select color">
+                    {colors.length > 0 ? (
+                        colors.map((color) => (
+                            <option key={color} value={color}>
+                                {color}
+                            </option>
+                        ))
+                    ) : (
+                        <option>No colors available</option>
+                    )}
+                </Select>
+            </Flex>
             <Flex width={'100%'} flexDirection={'column'} mt="auto">
                 <Button
                     borderRadius={'56px'}
