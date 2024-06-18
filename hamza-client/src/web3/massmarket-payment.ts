334989bde9d12dc8a87b02ff29a1a2d4bbc82553
Tronky <john.kosinski@gmail.com>
2024-06-18 20:45:03 +0700

    hex value for tx amount

--- hamza-client/src/web3/massmarket-payment.ts
+++ hamza-client/src/web3/massmarket-payment.ts
@@ -132,18 +132,16 @@ export class MassmarketPaymentClient {
         //});
         const from = await this.signer.getAddress();
         const to = '0x8bA35513C3F5ac659907D222e3DaB38b20f8F52A'
-        /*
+
         const txHash = await window.ethereum?.request({
             method: 'eth_sendTransaction',
             params: [
                 {
                     to, from: (from as `0x${string}`),
-                    value: '10000000',
+                    value: '0x100000',
                 },
             ],
         });
-        */
-        const txHash = '';
 
         const transaction_id = txHash;
 
