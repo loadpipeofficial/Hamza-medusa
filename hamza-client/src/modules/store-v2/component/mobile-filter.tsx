1316e5ad73ee5514bbfc4b7f2c14085f93bcc1b0
Jonatahn Bajada <jbajada@ryerson.ca>
2024-06-25 14:34:42 +0700

    mobile filter panel update

--- hamza-client/src/modules/store-v2/component/mobile-filter.tsx
+++ hamza-client/src/modules/store-v2/component/mobile-filter.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import mobileFilter from '../../../../public/images/categories/mobile-filter.svg';
-import { Flex, Box } from '@chakra-ui/react';
+import {
+    Flex,
+    Box,
+    Input,
+    Text,
+    InputGroup,
+    InputRightElement,
+} from '@chakra-ui/react';
 import Image from 'next/image';
+import { MdChevronLeft } from 'react-icons/md';
+import { FaSearch } from 'react-icons/fa';
+
 const MobileFilter = () => {
     return (
         <Flex
@@ -9,27 +19,62 @@ const MobileFilter = () => {
             display={{ base: 'flex', md: 'none' }}
             my="2rem"
             px="1rem"
+            gap={'12px'}
         >
-            <Flex>Search Bar</Flex>
-            <Flex
-                ml="auto"
-                borderWidth={'2px'}
-                width={'50px'}
-                height={'50px'}
-                borderRadius={'full'}
-                borderColor={'#555555'}
-                justifyContent={'center'}
-            >
-                <Image
-                    style={{
-                        width: '18px',
-                        height: '18px',
-                        alignSelf: 'center',
-                    }}
-                    src={mobileFilter}
-                    alt="mobile filter"
+            {/* back button */}
+            <Box alignSelf={'center'} width={'30px'} cursor={'pointer'}>
+                <MdChevronLeft
+                    color="white"
+                    size={40}
+                    style={{ marginRight: 'auto' }}
                 />
-            </Flex>
+            </Box>
+            {/* search bar */}
+            <InputGroup>
+                <Input
+                    width="100%"
+                    height="52px"
+                    borderWidth={'2px'}
+                    borderColor={'#3E3E3E'}
+                    borderRadius={'full'}
+                />
+                <InputRightElement
+                    height="100%"
+                    display="flex"
+                    alignItems="center"
+                    pointerEvents="none"
+                    mr="3"
+                >
+                    <FaSearch size={20} color="white" />
+                </InputRightElement>
+            </InputGroup>
+            {/* filter button */}
+            <Box>
+                <Flex
+                    wrap={'nowrap'}
+                    ml="auto"
+                    borderWidth={'2px'}
+                    width={'52px'}
+                    height={'52px'}
+                    borderRadius={'full'}
+                    borderColor={'#3E3E3E'}
+                    justifyContent={'center'}
+                    cursor={'pointer'}
+                    _hover={{
+                        borderColor: 'white',
+                    }}
+                >
+                    <Image
+                        style={{
+                            width: '18px',
+                            height: '18px',
+                            alignSelf: 'center',
+                        }}
+                        src={mobileFilter}
+                        alt="mobile filter"
+                    />
+                </Flex>
+            </Box>
         </Flex>
     );
 };
