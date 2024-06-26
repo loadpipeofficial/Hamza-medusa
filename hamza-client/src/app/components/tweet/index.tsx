0d21d0d60264a84cc6c934ed180bbda0443e6293
Tronky <john.kosinski@gmail.com>
2024-06-27 00:38:46 +0700

    tweet this product text next to X

--- hamza-client/src/app/components/tweet/index.tsx
+++ hamza-client/src/app/components/tweet/index.tsx
@@ -42,9 +42,20 @@ const Tweet: React.FC<TweetProps> = ({
                             <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
                         </svg>
                     </span>
+                </button>{' '}
+                <button
+                    type="button"
+                    data-twe-ripple-init
+                    data-twe-ripple-color="light"
+                    className="mb-2 inline-block rounded bg-black px-6 py-2.5 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg"
+                    disabled={isDisabled}
+                >
+                    <span className="[&>svg]:h-4 [&>svg]:w-4">
+                        Tweet this product
+                    </span>
                 </button>
             </a>
-        </div>
+        </div >
     );
 };
 
