bcd6d5997cdb24fadbfd210c77fa5f01b8f143e5
Tronky <john.kosinski@gmail.com>
2024-06-27 03:21:19 +0700

    default currency code enforced in formatCryptoPrice

--- hamza-client/src/lib/util/get-product-price.ts
+++ hamza-client/src/lib/util/get-product-price.ts
@@ -103,6 +103,8 @@ export function formatCryptoPrice(
     currencyCode: string
 ): string | number {
     try {
+        if (!currencyCode?.length)
+            currencyCode = 'usdc';
         if (!amount) amount = 0;
         const displayPrecision = getCurrencyPrecision(currencyCode).db ?? 2;
         amount = amount / 10 ** displayPrecision;
