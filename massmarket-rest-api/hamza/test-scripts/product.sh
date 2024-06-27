550bba431d8cb343a417ca6f511f85356a781444
Acer <john.kosinski@gmail.com>
2024-06-27 12:29:47 +0700

    update controllers/test scripts for latest version

--- massmarket-rest-api/hamza/test-scripts/product.sh
+++ massmarket-rest-api/hamza/test-scripts/product.sh
@@ -1,4 +1,4 @@
 curl --header "Content-Type: application/json" \
   --request POST \
-  --data '{"storeId":"0x382e9fdf10295e01ad4c7e4dc7e3cecf461016addbe8e15e55736983af39426c","keycard":"0x1d24055a9122ab48d62e9f65f37335f844d7de6858edf61d3ca115efbe82be86", "paymentCurrency":"0xbe9fe9b717c888a2b2ca0a6caa639afe369249c5", "products": [{"name": "Medusa Coffee Mug", "price": "10.00", "description": "Every programmers friend", "image": "https://medusa-public-images.s3.eu-west-1.amazonaws.com/coffee-mug.png" }]}' \
+  --data '{"storeId":"0xf9bd19671391ec9ce554eb7b788e5c661a5906b70fbbf6f4adbbd8383623b058","keycard":"0xd476aa67d4ab33576da3db6d75fd8e93782ba50b13a0fc3b442d05869e2612df", "products": [{"name": "Medusa Coffee Mug", "price": "10.00", "description": "Every programmers friend", "image": "https://medusa-public-images.s3.eu-west-1.amazonaws.com/coffee-mug.png" }]}' \
   http://localhost:3001/api/product
